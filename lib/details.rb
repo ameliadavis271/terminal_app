929dbc6d9fa4c913d2633efca01aac122885ac69
Amelia Davis <amelidavis271@gmail.com>
2020-09-28 02:39:53 -0700

    Displays weight difference when new weight entered

--- lib/details.rb
+++ lib/details.rb
@@ -6,6 +6,13 @@ module Details
     print '> '
     weight = gets.chomp.to_i
     system('clear')
+    if weight < user.weight
+      puts "CONGRATULATIONS ON YOUR WEIGHT LOSS!!!".colorize(:green)
+      puts "You've lost #{user.weight - weight} kgs!".colorize(:green)
+    else 
+      puts "Looks like you've put on #{weight - user.weight} kgs".colorize(:yellow)
+      puts "Try to exercise more regularly and maintain a healthy diet!".colorize(:green)
+    end
     user.weight = weight
     User.save_user(user)
   end
