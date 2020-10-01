965e2299f017bd487b74833de7c4026fe93bf3a1
Amelia Davis <amelidavis271@gmail.com>
2020-09-30 21:33:37 -0700

    non numeric error now works when updating weight

--- lib/details.rb
+++ lib/details.rb
@@ -2,21 +2,20 @@ module Details
   module_function
 
   def update_weight(user)
-    puts 'Please enter your new weight in kgs'
-    print '> '
-    weight = gets.chomp.to_i
+    weight = user.weight
+    user.weight = Errors.ask('Please enter your new weight in kgs')
     system('clear')
-    if weight < user.weight.to_i
+    if weight.to_i > user.weight.to_i
       Quotes.inspirational
       puts 'CONGRATULATIONS ON YOUR WEIGHT LOSS!!!'.colorize(:green)
-      puts "You've lost #{user.weight.to_i - weight} kgs!".colorize(:green)
-    elsif weight == user.weight.to_i
+      puts "You've lost #{weight.to_i - user.weight.to_i} kgs!".colorize(:green)
+    elsif weight.to_i == user.weight.to_i
       puts 'No changes to your weight at this time'.colorize(:yellow)
     else
       Quotes.motivational
-      puts "Looks like you've put on #{weight - user.weight.to_i} kgs".colorize(:red)
+      puts "Looks like you've put on #{user.weight.to_i - weight.to_i} kgs".colorize(:red)
     end
-    user.weight = weight
+    # user.weight = weight
     User.save_user(user)
   end
 end
